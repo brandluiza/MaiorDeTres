a3b93c238ec5e7c48af7ee8cda77f4b05814b263
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-09 21:37:14 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.WriteLine(" Maior De Três  \n");
+
+Console.ResetColor();
+
+Console.Write("Digite um Número: ");
+
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o próximo número: ");
+
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite outro número: ");
+
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int maiorNumero;
+
+if (n1 > n2)
+{
+    if (n1 > n3)
+    {
+        maiorNumero = n1;
+    }
+    else
+    {
+        maiorNumero = n3;
+    }
+}
+else
+{
+    if (n2 > n3)
+    {
+        maiorNumero = n2;
+    }
+    else
+    {
+        maiorNumero = n3;
+    }
+}
+
+Console.WriteLine($"\nMaior número: {maiorNumero}");
